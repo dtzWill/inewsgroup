610a0b5d138a6abe1f2c539fc17d0d3c16b55a84
willdtz <willdtz@e34bc176-103e-0410-998e-f989783027d5>
2007-12-09 07:55:05 +0000

    0.0.6 Release

--- trunk/ComposeView.h
+++ trunk/ComposeView.h
@@ -1,2 +1,40 @@
 //Will Dietz
 //ComposeView.h
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <UIKit/UIView.h>
+#import <UIKit/UITextView.h>
+
+//Prepare a post
+
+#define SUBJECT_ROW 0
+#define CONTENT_ROW ( SUBJECT_ROW + 1 )
+
+@interface PostView: UIView
+{
+	int _postnum, _groupnum;
+
+	UITextView * _textView;
+    NSMutableArray * _rows;// holds the row objects for our table 
+    UINavigationItem * _titleItem;//title
+    UITable * _table;
+	UIAlertSheet * _message;
+	id _delegate;
+}
+
+- (id) initWithFrame: (CGRect) rect;
+
+- (void) setArticleNum: (int) artnum andGroupnum: (int) groupnum;
+
+- (void) refresh;
+
+- (void) getPost;
+
+- (void) setDelegate: (id) delegate; //are YOU my mommmy?
+
+//- (void) returnToPage;
+
+
+@end
+
